935446c6c3fe566de98f2aee130e628191d4f723
avrahamcool <essoudry@gmail.com>
2018-01-31 22:47:19 +0200

    Aleph1.WebAPI.ExceptionHandler

---
+++ Aleph1.WebAPI.ExceptionHandler/ExceptionHandlerAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Http.Filters;
+
+namespace Aleph1.WebAPI.ExceptionHandler
+{
+    /// <summary>Let the Actions use a frendly message</summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class ExceptionHandlerAttribute : ExceptionFilterAttribute
+    {
+        /// <summary>The message to show for the client</summary>
+        public string CustomMessage { get; set; }
+
+        /// <summary>You have to specify a Custom message</summary>
+        /// <param name="CustomMessage">The message to show for the client</param>
+        public ExceptionHandlerAttribute(string CustomMessage)
+        {
+            this.CustomMessage = CustomMessage;
+        }
+
+
+        /// <summary>replace the current Exception with a new one</summary>
+        /// <param name="actionExecutedContext"></param>
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            actionExecutedContext.Exception = new Exception(CustomMessage, actionExecutedContext.Exception);
+        }
+    }
+}
