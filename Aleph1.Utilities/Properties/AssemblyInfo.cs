0d4c71e7598d42f3f055546c1a915d5c8ed51a39
avrahamcool <essoudry@gmail.com>
2018-01-31 22:22:26 +0200

    Aleph1.Utilities

---
+++ Aleph1.Utilities/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aleph1.Utilities")]
+[assembly: AssemblyDescription("Common Utilities for Aleph1 projects")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aleph1")]
+[assembly: AssemblyProduct("Aleph1.Utilities")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ff1b982-b380-4aa6-a7d2-5d298679402c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Aleph1.Utilities/UserExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+using System.Web;
+
+namespace Aleph1.Utitilies
+{
+    /// <summary>Handles common User related tasks (such as, getting the current user logon name)</summary>
+    public static class UserExtentions
+    {
+        /// <summary>Get the current user logon name</summary>
+        public static string CurrentUserName
+        {
+            get
+            {
+                return HttpContext.Current?.User?.Identity?.Name ?? WindowsIdentity.GetCurrent()?.Name ?? string.Empty;
+            }
+        }
+    }
+}
