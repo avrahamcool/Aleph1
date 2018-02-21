259a6e4728683ac409a609a14ed599b8e69537ae
avrahamcool <essoudry@gmail.com>
2018-02-21 21:06:14 +0200

    Updated security

--- Aleph1.Security.Implementation.3DES/TripleDES.cs
+++ Aleph1.Security.Implementation.3DES/TripleDES.cs
@@ -1,5 +1,6 @@
 ﻿using Aleph1.Security.Contracts;
 using System;
+using System.Security.Cryptography;
 using System.Text;
 using System.Web.Script.Serialization;
 
@@ -26,11 +27,9 @@ namespace Aleph1.Security.Implementation._3DES
             public Storage() { }
             public Storage(T data, TimeSpan? timeSpan)
             {
-                this.Data = data;
+                Data = data;
                 if (timeSpan.HasValue)
-                {
-                    this.ExpirationDate = DateTime.UtcNow + timeSpan.Value;
-                }
+                    ExpirationDate = DateTime.UtcNow + timeSpan.Value;
             }
 
             public T Data { get; set; }
@@ -39,14 +38,58 @@ namespace Aleph1.Security.Implementation._3DES
             public DateTime? ExpirationDate { get; set; }
         }
 
+        /// <summary>Decrypts the specified data for a specific user in a spesific application.</summary>
+        /// <typeparam name="T">Any</typeparam>
+        /// <param name="appPrefix">The application prefix.</param>
+        /// <param name="userUniqueID">The user unique identifier.</param>
+        /// <param name="encryptedData">The encrypted data.</param>
+        /// <returns>the decrypted data</returns>
+        /// <exception cref="CryptographicException"></exception>
         public T Decrypt<T>(string appPrefix, string userUniqueID, string encryptedData)
         {
-            throw new NotImplementedException();
+            using (TripleDESCryptoServiceProvider cryptoService = new TripleDESCryptoServiceProvider())
+            {
+                cryptoService.Mode = CipherMode.ECB;
+                cryptoService.Padding = PaddingMode.PKCS7;
+                cryptoService.Key = GetKey(appPrefix, userUniqueID);
+                using (ICryptoTransform decryptor = cryptoService.CreateDecryptor())
+                {
+                    byte[] buffer = Convert.FromBase64String(encryptedData.Replace(' ', '+'));
+
+                    //Throws error when invalid
+                    string serizlizedTicket = Encoding.UTF8.GetString(decryptor.TransformFinalBlock(buffer, 0, buffer.Length));
+
+                    Storage<T> store = TEXT_SERIALIZER.Deserialize<Storage<T>>(serizlizedTicket);
+                    if (store.ExpirationDate.HasValue && store.ExpirationDate.Value < DateTime.UtcNow)
+                        throw new CryptographicException($"Data Expired {DateTime.UtcNow - store.ExpirationDate.Value} ago");
+
+                    return store.Data;
+                }
+            }
         }
 
+        /// <summary>Encrypts the specified data for a specific user in a specific application.</summary>
+        /// <typeparam name="T">Any</typeparam>
+        /// <param name="appPrefix">The application prefix.</param>
+        /// <param name="userUniqueID">The user unique identifier.</param>
+        /// <param name="data">The data to encrypt</param>
+        /// <param name="timeSpan">the duration of the ticket generated</param>
+        /// <returns>the encrypted data</returns>
         public string Encrypt<T>(string appPrefix, string userUniqueID, T data, TimeSpan? timeSpan = null)
         {
-            throw new NotImplementedException();
+            using (TripleDESCryptoServiceProvider cryptoService = new TripleDESCryptoServiceProvider())
+            {
+                cryptoService.Mode = CipherMode.ECB;
+                cryptoService.Padding = PaddingMode.PKCS7;
+                cryptoService.Key = GetKey(appPrefix, userUniqueID);
+
+                using (ICryptoTransform encryptor = cryptoService.CreateEncryptor())
+                {
+                    Storage<T> store = new Storage<T>(data, timeSpan);
+                    byte[] buffer = Encoding.UTF8.GetBytes(TEXT_SERIALIZER.Serialize(store));
+                    return Convert.ToBase64String(encryptor.TransformFinalBlock(buffer, 0, buffer.Length));
+                }
+            }
         }
     }
 }
--- Aleph1.Skeletons.WebAPI/Tools/init.ps1
+++
@@ -1,7 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$project.ProjectItems.Item("ModuleInit.cs").Delete()
-$project.ProjectItems.Item("App_Start").ProjectItems.Item("UnityWebApiActivator.cs").Delete()
-
-Uninstall-Package Microsoft.CodeDom.Providers.DotNetCompilerPlatform -ProjectName $project.ProjectName
-Uninstall-Package Microsoft.Net.Compilers -ProjectName $project.ProjectName
