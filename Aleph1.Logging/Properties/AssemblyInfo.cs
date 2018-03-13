721a329eef4506103986a9e7442e93d04097fc24
Avraham Essoudry <essoudry@gmail.com>
2018-03-13 20:41:26 +0200

    Multiplatform Utilities Downgrade Logging Multiplatform ExceptionHandler

--- Aleph1.Logging/Properties/AssemblyInfo.cs
+++ Aleph1.Logging/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aleph1")]
 [assembly: AssemblyProduct("Aleph1.Logging")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright Aleph1 ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
