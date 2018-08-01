0ac90ca32430cda348bf1ef86cb1474e35e9d225
Avraham Essoudry <essoudry@gmail.com>
2018-08-01 14:06:34 +0300

    userName defaults to IP if name is empty

--- Aleph1.Utilities/UserExtentions.cs
+++ Aleph1.Utilities/UserExtentions.cs
@@ -7,11 +7,12 @@ namespace Aleph1.Utitilies
     public static class UserExtentions
     {
         /// <summary>Get the current user logon name</summary>
+        /// <remarks>Identity from HttpContext, Identity from Windows Context => IP from HttpContext => Empty string</remarks>
         public static string CurrentUserName
         {
             get
             {
-                return HttpContext.Current?.User?.Identity?.Name ?? WindowsIdentity.GetCurrent()?.Name ?? string.Empty;
+                return HttpContext.Current?.User?.Identity?.Name ?? WindowsIdentity.GetCurrent()?.Name ?? HttpContext.Current?.Request.UserHostAddress ?? string.Empty;
             }
         }
     }
