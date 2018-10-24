c63d1dc74057365dd272ed6562ec4c91d3e9e17e
Avraham Essoudry <essoudry@gmail.com>
2018-10-24 18:18:49 +0300

    aded net40 support

---
+++ Aleph1.Logging/net40fix.cs
@@ -0,0 +1,22 @@
+﻿#if NET40
+namespace System.Runtime.CompilerServices
+{
+    /// <summary></summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public class CallerMemberNameAttribute : Attribute
+    {
+    }
+    
+    /// <summary></summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public class CallerFilePathAttribute : Attribute
+    {
+    }
+
+    /// <summary></summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public class CallerLineNumberAttribute : Attribute
+    {
+    }
+}
+#endif
