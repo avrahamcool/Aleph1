c41af1328cb5e161780b5ec3fc2778d1ff4f450c
Avraham Essoudry <essoudry@gmail.com>
2019-07-10 17:23:11 +0300

    make fields public set + restore default ctor

--- Aleph1.ClientFile.Models/ClientFile.cs
+++ Aleph1.ClientFile.Models/ClientFile.cs
@@ -4,34 +4,19 @@
     /// <remarks>https://developer.mozilla.org/en-US/docs/Web/API/File</remarks>
     public class ClientFile
     {
-        /// <summary>this constructor should not be use directly - it is present for the JSON serializer.</summary>
-        /// <param name="name">the name of the file</param>
-        /// <param name="lastModified">the last modified time of the file, in millisecond since the UNIX epoch (January 1st, 1970 at Midnight).</param>
-        /// <param name="size">the size of the file in bytes.</param>
-        /// <param name="type">the MIME type of the file.</param>
-        /// <param name="content">the file itself.</param>
-        public ClientFile(string name, long lastModified, long size, string type, byte[] content)
-        {
-            this.Name = name;
-            this.LastModified = lastModified;
-            this.Size = size;
-            this.Type = type;
-            this.Content = content;
-        }
-
         /// <summary>the name of the file</summary>
-        public string Name { get; private set; }
+        public string Name { get; set; }
 
         /// <summary>the last modified time of the file, in millisecond since the UNIX epoch (January 1st, 1970 at Midnight).</summary>
-        public long LastModified { get; private set; }
+        public long LastModified { get; set; }
 
         /// <summary>the size of the file in bytes.</summary>
-        public long Size { get; private set; }
+        public long Size { get; set; }
 
         /// <summary>the MIME type of the file.</summary>
-        public string Type { get; private set; }
+        public string Type { get; set; }
 
         /// <summary>the file itself.</summary>
-        public byte[] Content { get; private set; }
+        public byte[] Content { get; set; }
     }
 }
