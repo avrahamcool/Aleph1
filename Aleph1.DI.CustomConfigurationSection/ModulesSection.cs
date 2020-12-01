3fe5b6c5501d5a14620a6916ad168fd751f309ff
Avraham Essoudry <essoudry@gmail.com>
2020-12-01 17:47:45 +0200

    closes https://github.com/avrahamcool/Aleph1/issues/59

--- Aleph1.DI.CustomConfigurationSection/ModulesSection.cs
+++ Aleph1.DI.CustomConfigurationSection/ModulesSection.cs
@@ -7,6 +7,14 @@ namespace Aleph1.DI.CustomConfigurationSection
     /// <seealso cref="ConfigurationSection" />
     public class ModulesSection : ConfigurationSection
     {
+        /// <summary>path to the modules directory</summary>
+        [ConfigurationProperty("base")]
+        public BaseElement Base
+        {
+            get => (BaseElement)(base["base"]);
+            set => base["base"] = value;
+        }
+
         /// <summary>collection of modules elements</summary>
         [ConfigurationProperty("modules")]
         public ModuleElementCollection Modules
@@ -22,25 +30,37 @@ namespace Aleph1.DI.CustomConfigurationSection
     public class ModuleElementCollection : ConfigurationElementCollection
     {
         /// <summary>Gets the type of the <see cref="T:System.Configuration.ConfigurationElementCollection" />.</summary>
-        public override ConfigurationElementCollectionType CollectionType { get => ConfigurationElementCollectionType.BasicMapAlternate; }
+        public override ConfigurationElementCollectionType CollectionType => ConfigurationElementCollectionType.BasicMapAlternate;
 
         /// <summary>Gets the name used to identify this collection of elements in the configuration file when overridden in a derived class.</summary>
-        protected override string ElementName { get => "module"; }
+        protected override string ElementName => "module";
 
         /// <summary>Indicates whether the specified <see cref="T:System.Configuration.ConfigurationElement" /> exists in the <see cref="T:System.Configuration.ConfigurationElementCollection" />.</summary>
         /// <param name="elementName">The name of the element to verify.</param>
-        protected override bool IsElementName(string elementName) => elementName.Equals("module", StringComparison.InvariantCultureIgnoreCase);
+        protected override bool IsElementName(string elementName)
+        {
+            return elementName.Equals("module", StringComparison.InvariantCultureIgnoreCase);
+        }
 
         /// <summary>Indicates whether the <see cref="T:System.Configuration.ConfigurationElementCollection" /> object is read only.</summary>
-        public override bool IsReadOnly() => false;
+        public override bool IsReadOnly()
+        {
+            return false;
+        }
 
         /// <summary>When overridden in a derived class, creates a new <see cref="T:System.Configuration.ConfigurationElement" />.</summary>
-        protected override ConfigurationElement CreateNewElement() => new ModuleElement();
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new ModuleElement();
+        }
 
         /// <summary>Gets the element key for a specified configuration element when overridden in a derived class.</summary>
         /// <param name="element">The <see cref="T:System.Configuration.ConfigurationElement" /> to return the key for.</param>
         /// <returns>An <see cref="T:System.Object" /> that acts as the key for the specified <see cref="T:System.Configuration.ConfigurationElement" />.</returns>
-        protected override object GetElementKey(ConfigurationElement element) => ((ModuleElement)element).Path;
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((ModuleElement)element).Path;
+        }
 
         /// <summary>Gets the <see cref="ModuleElement"/> with the specified index.</summary>
         /// <value>The <see cref="ModuleElement"/>.</value>
@@ -49,10 +69,19 @@ namespace Aleph1.DI.CustomConfigurationSection
     }
 
     /// <summary>a single Module to load</summary>
-    /// <seealso cref="System.Configuration.ConfigurationElement" />
+    /// <seealso cref="ConfigurationElement" />
     public class ModuleElement : ConfigurationElement
     {
-        /// <summary>Gets the path to the module/// </summary>
+        /// <summary>Gets the path to the module</summary>
+        [ConfigurationProperty("path", IsRequired = true, IsKey = true)]
+        public string Path => (string)base["path"];
+    }
+
+    /// <summary>path to the modules directory</summary>
+    /// <seealso cref="ConfigurationElement" />
+    public class BaseElement : ConfigurationElement
+    {
+        /// <summary>path to the modules directory</summary>
         [ConfigurationProperty("path", IsRequired = true, IsKey = true)]
         public string Path => (string)base["path"];
     }
