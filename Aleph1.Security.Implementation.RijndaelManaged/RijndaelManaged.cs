ab4be2e2522aaf40613d995cc967827bfa14c37b
Avraham Essoudry <essoudry@gmail.com>
2024-03-27 16:58:49 +0200

    fix encryption for .net 8

--- Aleph1.Security.Implementation.RijndaelManaged/RijndaelManaged.cs
+++ Aleph1.Security.Implementation.RijndaelManaged/RijndaelManaged.cs
@@ -14,10 +14,10 @@ namespace Aleph1.Security.Implementation.RijndaelManagedCipher
 	/// <seealso cref="ICipher" />
 	public class RijndaelManagedCipher : ICipher
 	{
-		// This constant is used to determine the key size of the encryption algorithm in bits.
-		// We divide this by 8 within the code below to get the equivalent number of bytes.
+		private const int BLOCK_SIZE = 128;
+		private const int BLOCK_SIZE_BYTES = BLOCK_SIZE / 8;
+
 		private const int KEY_SIZE = 256;
-		private const int KEY_SIZE_BYTES = KEY_SIZE / 8;
 
 		// This constant determines the number of iterations for the password bytes generation function.
 		private const int DERIVATION_ITERATIONS = 1000;
@@ -48,9 +48,10 @@ namespace Aleph1.Security.Implementation.RijndaelManagedCipher
 			public string UserUniqueID { get; set; }
 		}
 
-		private static byte[] GenerateBitsOfRandomEntropy()
+#if NET48
+		private static byte[] GenerateBitsOfRandomEntropy(int size)
 		{
-			byte[] randomBytes = new byte[KEY_SIZE_BYTES];
+			byte[] randomBytes = new byte[size];
 
 			// Fill the array with cryptographically secure random bytes.
 			using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
@@ -60,76 +61,19 @@ namespace Aleph1.Security.Implementation.RijndaelManagedCipher
 
 			return randomBytes;
 		}
-
-		/// <summary>Decrypts the specified data for a specific user in a specific application.</summary>
-		/// <typeparam name="T">Any</typeparam>
-		/// <param name="appPrefix">The application prefix.</param>
-		/// <param name="userUniqueID">The user unique identifier.</param>
-		/// <param name="encryptedData">The encrypted data.</param>
-		/// <returns>the decrypted data</returns>
-		public T Decrypt<T>(string appPrefix, string userUniqueID, string encryptedData)
+#endif
+#if NET8_0
+		private static byte[] GenerateBitsOfRandomEntropy(int size)
 		{
-			if (string.IsNullOrWhiteSpace(appPrefix) || string.IsNullOrWhiteSpace(userUniqueID) || string.IsNullOrWhiteSpace(encryptedData))
+			return RandomNumberGenerator.GetBytes(size);
+		}
+#endif
+
+		private static byte[] GenerateKey(string appPrefix, string userUniqueID, byte[] salt)
+		{
+			using (Rfc2898DeriveBytes deriveBytes = new Rfc2898DeriveBytes(appPrefix + userUniqueID, salt, DERIVATION_ITERATIONS, HashAlgorithmName.SHA512))
 			{
-				throw new CryptographicException("empty appPrefix or userUniqueID or encryptedData");
-			}
-
-			string passPhrase = appPrefix + userUniqueID;
-
-			// Get the complete stream of bytes that represent:
-			// [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
-			byte[] cipherTextBytesWithSaltAndIv = Convert.FromBase64String(encryptedData);
-
-			// Get the salt bytes by extracting the first KEY_SIZE_BYTES bytes from the supplied cipherText bytes.
-			byte[] saltStringBytes = cipherTextBytesWithSaltAndIv.Take(KEY_SIZE_BYTES).ToArray();
-
-			// Get the IV bytes by extracting the next KEY_SIZE_BYTES bytes from the supplied cipherText bytes.
-			byte[] ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(KEY_SIZE_BYTES).Take(KEY_SIZE_BYTES).ToArray();
-
-			// Get the actual cipher text bytes by removing the first 2 * KEY_SIZE_BYTES bytes from the cipherText string.
-			byte[] cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip(KEY_SIZE_BYTES * 2).Take(cipherTextBytesWithSaltAndIv.Length - (KEY_SIZE_BYTES * 2)).ToArray();
-
-			using (Rfc2898DeriveBytes password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DERIVATION_ITERATIONS))
-			{
-				byte[] keyBytes = password.GetBytes(KEY_SIZE_BYTES);
-				using (RijndaelManaged symmetricKey = new RijndaelManaged())
-				{
-					symmetricKey.BlockSize = KEY_SIZE;
-					symmetricKey.Mode = CipherMode.CBC;
-					symmetricKey.Padding = PaddingMode.PKCS7;
-					using (ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
-					{
-						using (MemoryStream memoryStream = new MemoryStream(cipherTextBytes))
-						{
-							using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-							{
-								byte[] plainTextBytes = new byte[cipherTextBytes.Length];
-								int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-								memoryStream.Close();
-								cryptoStream.Close();
-
-								//Throws error when invalid
-								string serizlizedTicket = Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
-
-								Storage<T> store = JsonConvert.DeserializeObject<Storage<T>>(serizlizedTicket);
-
-								//Ticket Decrypted successfully - but not for the right user
-								if (store.AppPrefix != appPrefix || store.UserUniqueID != userUniqueID)
-								{
-									throw new CryptographicException($"Ticket Violation");
-								}
-
-								//Ticket Decrypted successfully - but expired
-								if (store.ExpirationDate.HasValue && store.ExpirationDate.Value < DateTime.UtcNow)
-								{
-									throw new CryptographicException($"Data Expired {DateTime.UtcNow - store.ExpirationDate.Value} ago");
-								}
-
-								return store.Data;
-							}
-						}
-					}
-				}
+				return deriveBytes.GetBytes(KEY_SIZE / 8);
 			}
 		}
 
@@ -149,41 +93,101 @@ namespace Aleph1.Security.Implementation.RijndaelManagedCipher
 
 			Storage<T> store = new Storage<T>(data, appPrefix, userUniqueID, timeSpan);
 			string plainText = JsonConvert.SerializeObject(store);
-			string passPhrase = appPrefix + userUniqueID;
+
 
 			// Salt and IV are randomly generated each time, and added to encrypted cipher text
 			// so that the same Salt and IV values can be used when decrypting.
-			byte[] saltStringBytes = GenerateBitsOfRandomEntropy();
-			byte[] ivStringBytes = GenerateBitsOfRandomEntropy();
-			byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-			using (Rfc2898DeriveBytes password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DERIVATION_ITERATIONS))
+			byte[] salt = GenerateBitsOfRandomEntropy(BLOCK_SIZE_BYTES);
+			byte[] iv = GenerateBitsOfRandomEntropy(BLOCK_SIZE_BYTES);
+			byte[] key = GenerateKey(appPrefix, userUniqueID, salt);
+
+
+			using (Aes aesAlg = Aes.Create())
 			{
-				byte[] keyBytes = password.GetBytes(KEY_SIZE_BYTES);
-				using (RijndaelManaged symmetricKey = new RijndaelManaged())
+				aesAlg.Key = key;
+				aesAlg.IV = iv;
+
+				// Create an encryptor to perform the stream transform
+				ICryptoTransform encryptor = aesAlg.CreateEncryptor();
+
+				byte[] cipherTextBytes;
+				// Create the streams used for encryption
+				using (MemoryStream msEncrypt = new MemoryStream())
 				{
-					symmetricKey.BlockSize = KEY_SIZE;
-					symmetricKey.Mode = CipherMode.CBC;
-					symmetricKey.Padding = PaddingMode.PKCS7;
-					using (ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+					using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
 					{
-						using (MemoryStream memoryStream = new MemoryStream())
+						using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
 						{
-							using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-							{
-								cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-								cryptoStream.FlushFinalBlock();
-								// Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
-								byte[] cipherTextBytes = saltStringBytes;
-								cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
-								cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
-								memoryStream.Close();
-								cryptoStream.Close();
-								return Convert.ToBase64String(cipherTextBytes);
-							}
+							// Write all data to the stream
+							swEncrypt.Write(plainText);
 						}
 					}
+					cipherTextBytes = msEncrypt.ToArray();
+				}
+
+				return Convert.ToBase64String(salt.Concat(iv).Concat(cipherTextBytes).ToArray());
+			}
+		}
+
+		/// <summary>Decrypts the specified data for a specific user in a specific application.</summary>
+		/// <typeparam name="T">Any</typeparam>
+		/// <param name="appPrefix">The application prefix.</param>
+		/// <param name="userUniqueID">The user unique identifier.</param>
+		/// <param name="encryptedData">The encrypted data.</param>
+		/// <returns>the decrypted data</returns>
+		public T Decrypt<T>(string appPrefix, string userUniqueID, string encryptedData)
+		{
+			if (string.IsNullOrWhiteSpace(appPrefix) || string.IsNullOrWhiteSpace(userUniqueID) || string.IsNullOrWhiteSpace(encryptedData))
+			{
+				throw new CryptographicException("empty appPrefix or userUniqueID or encryptedData");
+			}
+
+			// Get the complete stream of bytes that represent:
+			// [BLOCK_SIZE_BYTES bytes of Salt] + [BLOCK_SIZE_BYTES bytes of IV] + [n bytes of CipherText]
+			byte[] saltIvAndCipherTextBytes = Convert.FromBase64String(encryptedData);
+
+			// Get the salt bytes by extracting the first BLOCK_SIZE_BYTES bytes from the supplied saltIvAndCipherTextBytes bytes.
+			byte[] salt = saltIvAndCipherTextBytes.Take(BLOCK_SIZE_BYTES).ToArray();
+
+			// Get the IV bytes by extracting the next BLOCK_SIZE_BYTES bytes from the supplied saltIvAndCipherTextBytes bytes.
+			byte[] iv = saltIvAndCipherTextBytes.Skip(BLOCK_SIZE_BYTES).Take(BLOCK_SIZE_BYTES).ToArray();
+
+			// Get the actual cipher text bytes by removing the first 2 * BLOCK_SIZE_BYTES bytes from the saltIvAndCipherTextBytes string.
+			byte[] cipherText = saltIvAndCipherTextBytes.Skip(BLOCK_SIZE_BYTES * 2).ToArray();
+
+			byte[] key = GenerateKey(appPrefix, userUniqueID, salt);
+
+			string serizlizedTicket;
+			using (Aes aesAlg = Aes.Create())
+			{
+				aesAlg.Key = key;
+				aesAlg.IV = iv;
+
+				// Create an encryptor to perform the stream transform
+				ICryptoTransform decryptor = aesAlg.CreateDecryptor();
+
+				// Create the streams used for decryption
+				using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+				using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+				using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+				{ 
+					// Read the decrypted bytes from the decrypting stream
+					serizlizedTicket = srDecrypt.ReadToEnd();
 				}
 			}
+
+			Storage<T> store = JsonConvert.DeserializeObject<Storage<T>>(serizlizedTicket);
+
+			//Ticket Decrypted successfully - but not for the right user
+			if (store.AppPrefix != appPrefix || store.UserUniqueID != userUniqueID)
+			{
+				throw new CryptographicException($"Ticket Violation");
+			}
+
+			//Ticket Decrypted successfully - but expired
+			return store.ExpirationDate.HasValue && store.ExpirationDate.Value < DateTime.UtcNow
+				? throw new CryptographicException($"Data Expired {DateTime.UtcNow - store.ExpirationDate.Value} ago")
+				: store.Data;
 		}
 	}
 }
---
+++ Aleph1.Security.Tests.Core/CipherTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Security.Cryptography;
+
+using Aleph1.Security.Contracts;
+
+using Fastenshtein;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Newtonsoft.Json;
+
+namespace Aleph1.Security.Tests.Core
+{
+	[TestClass]
+	public class CipherTests
+	{
+		private readonly ICipher cipher = new Implementation.RijndaelManagedCipher.RijndaelManagedCipher();
+		private readonly string secret = "My special secret - hello world";
+		private readonly string appPrefix = "{3EE06365-D5E3-4D2E-A8D0-1F6E10138D29}";
+		private readonly string userUniqueID = "127.0.0.0";
+		private readonly string wrongAppPrefix = "{4EE06365-D5E3-4D2E-A8D0-1F6E10138D29}";
+		private readonly string wrongUserUniqueID = "127.1.0.0";
+
+		[TestMethod]
+		public void Decrypd_RightApp_RightClient_RightTime_Should_Work()
+		{
+			string ticket = cipher.Encrypt(appPrefix, userUniqueID, secret, TimeSpan.FromMinutes(1));
+
+			Assert.AreEqual(secret, cipher.Decrypt<string>(appPrefix, userUniqueID, ticket));
+		}
+
+		[TestMethod]
+		public void Decrypd_RightApp_RightClient_UnlimitedTime_Should_Work()
+		{
+			string ticket = cipher.Encrypt(appPrefix, userUniqueID, secret);
+
+			Assert.AreEqual(secret, cipher.Decrypt<string>(appPrefix, userUniqueID, ticket));
+		}
+
+		[TestMethod]
+		public void Decrypd_RightApp_RightClient_WrongTime_Should_Fail()
+		{
+			string ticket = cipher.Encrypt(appPrefix, userUniqueID, secret, TimeSpan.FromMilliseconds(15));
+			Thread.Sleep(30);
+
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(appPrefix, userUniqueID, ticket));
+		}
+
+		[TestMethod]
+		public void Decrypd_RightApp_WrongClient_Should_Fail()
+		{
+			string ticket = cipher.Encrypt(appPrefix, userUniqueID, secret);
+
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(appPrefix, wrongUserUniqueID, ticket));
+		}
+
+		[TestMethod]
+		public void Decrypd_WrongtApp_Should_Fail()
+		{
+			string ticket = cipher.Encrypt(appPrefix, userUniqueID, secret);
+
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(wrongAppPrefix, userUniqueID, ticket));
+		}
+
+		[TestMethod]
+		public void Decrypd_NullsAndEmptyStrings()
+		{
+			string ticket = cipher.Encrypt(appPrefix, userUniqueID, secret);
+
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>("", "", ticket));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(null, "", ticket));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>("", null, ticket));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(null, null, ticket));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(appPrefix, userUniqueID, ""));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(appPrefix, userUniqueID, null));
+		}
+
+		[TestMethod]
+		public void Decrypd_ChangingOneLetter_Should_Fail()
+		{
+			string ticket = cipher.Encrypt(appPrefix, userUniqueID, secret);
+
+			string someChar = ticket[ticket.Length / 2] == 'a' ? "b" : "a";
+			string wrongTicket = ticket.Remove(20, 1).Insert(1, someChar);
+
+			Assert.ThrowsException<CryptographicException>(() => cipher.Decrypt<string>(appPrefix, userUniqueID, wrongTicket));
+		}
+
+
+		[TestMethod]
+		public void Encrypt_NullsAndEmptyStrings()
+		{
+			Assert.ThrowsException<CryptographicException>(() => cipher.Encrypt("", "", secret));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Encrypt(null, "", secret));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Encrypt("", null, secret));
+			Assert.ThrowsException<CryptographicException>(() => cipher.Encrypt(null, null, secret));
+
+			string emptyStringCipher = cipher.Encrypt(appPrefix, userUniqueID, "");
+			Assert.AreEqual("", cipher.Decrypt<string>(appPrefix, userUniqueID, emptyStringCipher));
+
+			string nullCipher = cipher.Encrypt<string>(appPrefix, userUniqueID, null);
+			Assert.IsNull(cipher.Decrypt<string>(appPrefix, userUniqueID, nullCipher));
+		}
+
+		[TestMethod]
+		public void Encrypt_DifferentTimes_ShouldResultInBigVariant()
+		{
+			string ticket1 = cipher.Encrypt(appPrefix, userUniqueID, secret, TimeSpan.FromMinutes(1));
+			string ticket2 = cipher.Encrypt(appPrefix, userUniqueID, secret, TimeSpan.FromMinutes(15));
+			int distance = Levenshtein.Distance(ticket1, ticket2);
+
+			Assert.IsTrue(distance > 0.7 * ticket1.Length, "encrypted text don't vary enough");
+		}
+	}
+}
